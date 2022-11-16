cc5d574ccdcffa18a3975a83c6902e16993c5868
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-11-16 15:28:02 +0000

    [Update]ユーザー側ユーザー機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
 
-  devise_for :users,skip: [:passwords]
-  root :to => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+  root :to => 'public/homes#top'
+
+  # users
+  get 'users' => 'users#show', as: 'mypage'
+  get 'users/information/edit' => 'users#edit', as: 'mypage_edit'
+  patch 'users/information' => 'users#update'
+  get 'users/confirm' => 'users#confirm'
+  patch 'users/withdraw' => 'users#withdraw'
+
+  end
+
 
 end
