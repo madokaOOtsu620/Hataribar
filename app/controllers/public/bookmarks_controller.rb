73df9acb34137cc87af0cd4351826af64758b05f
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-11-25 12:06:44 +0000

    before_actionを追加

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,4 +1,5 @@
 class Public::BookmarksController < ApplicationController
+  before_action :move_to_signed_in
 
   def create
     @post = Post.find(params[:post_id])
@@ -18,6 +19,12 @@ class Public::BookmarksController < ApplicationController
     @bookmarks = Bookmark.where(user_id: current_user.id)
     # 業界検索テンプレート用
     @industries = Industry.all
+  end
+
+  def move_to_signed_in
+    unless user_signed_in?
+      redirect_to new_user_session_path, notice: '！ ブックマークやコメントをしたい場合はログインが必要です。 ！'
+    end
   end
 
 end
